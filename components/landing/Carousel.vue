0cde7e29175844213f8a8feef8b74513ecc645b9
shenshuai_dr <shen-sh@neusoft.com>
2025-07-22 16:16:04 +0800

    feat: Carousel 轮播组件开发

---
+++ components/landing/Carousel.vue
@@ -0,0 +1,76 @@
+<template>
+  <Carousel
+    :value="products"
+    :responsiveOptions="responsiveOptions"
+    circular
+    :autoplayInterval="3000"
+    :showIndicators="false"
+    class="custom-carousel w-full"
+  >
+    <template #item="slotProps">
+      <div class="dark:border-surface-700 relative w-full rounded bg-slate-400">
+        <div class="absolute top-1/4 left-[5%] w-1/3">
+          <p class="text-7xl font-bold text-white">{{ slotProps.data.title }}</p>
+          <p class="mt-4 text-2xl font-normal text-white">{{ slotProps.data.desc }}</p>
+          <Button class="mt-8" label="Learn more" />
+        </div>
+        <img :src="slotProps.data.image" mode="scaleToFill" />
+      </div>
+    </template>
+  </Carousel>
+</template>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import Carousel from 'primevue/carousel';
+  import image1 from '~/assets/images/slider1.jpg';
+  import image2 from '~/assets/images/slider2.jpg';
+
+  const products = ref([
+    {
+      title: 'High quality design',
+      desc: 'But in practice, as many differences exist within these is resolve similar problems in the future.',
+      image: image1,
+    },
+    {
+      title: 'Digital product',
+      desc: 'But in practice, as many differences exist within these is resolve similar problems in the future.',
+      image: image2,
+    },
+  ]);
+
+  const responsiveOptions = ref([
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '600px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '480px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ]);
+</script>
+
+<style scoped>
+  :deep(.p-carousel-content) {
+    position: relative;
+    width: 100%;
+  }
+  :deep(.p-carousel-prev-button) {
+    top: 50%;
+    position: absolute !important;
+    z-index: 999;
+  }
+  :deep(.p-carousel-next-button) {
+    top: 50%;
+    right: 0px;
+    position: absolute !important;
+    z-index: 999;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,10 @@
 <template>
-  <main class="min-h-[calc(100vh-88px)]">
-    <div class="container mx-auto px-4 py-8">
-      <h1 class="text-3xl font-bold text-green-600 dark:text-white"> 欢迎使用 </h1>
-      <p class="mt-4"> 这是一个支持暗黑模式的页面 </p>
-    </div>
-  </main>
+  <LandingCarousel />
 </template>
 
 <script setup lang="ts">
   import { ref } from 'vue';
+
   const date = ref(new Date());
 </script>
 <style lang="scss" scoped></style>
